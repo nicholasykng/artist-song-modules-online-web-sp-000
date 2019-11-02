0b95bc3d13781f64aa468dc47cabe412154c75a7
Nicholas Ng <nicholasykng@gmail.com>
2019-11-02 23:16:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,13 +18,7 @@ class Song
     @@songs
   end
 extend Memorable::ClassMethods
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-#  end
+  
 
   def artist=(artist)
     @artist = artist
