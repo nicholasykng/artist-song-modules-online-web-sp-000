14cd99e53cc6cf81a0e0b604cc05d9b1efb1e5fa
Nicholas Ng <nicholasykng@gmail.com>
2019-11-02 23:19:49 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,7 @@
 module Findable
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+  module ClassMethods
+  def find_by_name(name)
+    self.detect{|a| a.name}
   end
-    def self.find_by_name(name)
-      @@artists.detect{|a| a.name == name}
-    end
-
-  end
+end
+end
