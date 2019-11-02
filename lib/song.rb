1c8a8e9282edbfdca921916d538b1ceeed9ec08a
Nicholas Ng <nicholasykng@gmail.com>
2019-11-02 23:32:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ extend Memorable::ClassMethods
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end
